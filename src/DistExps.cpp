eaecfc12b1bddc7f2bd4231b37d738ba6de9a7f3
SpatLyu <lyu.geosocial@gmail.com>
2026-03-18 00:00:01 +0800

    export distance utility

--- src/DistExps.cpp
+++ src/DistExps.cpp
@@ -5,7 +5,7 @@
 #include <numeric>
 #include <algorithm>
 #include "distance.hpp"
-#include <Rcpp.h>
+#include "DataTrans.h"
 
 // Wrapper function to compute the distance between two vectors
 // [[Rcpp::export(rng = false)]]
@@ -33,30 +33,13 @@ Rcpp::NumericVector RcppDist4Mat(
     bool na_rm = true
 ) {
     // Convert Rcpp::NumericMatrix to std::vector<std::vector<double>>
-    int numRows = mat.nrow();
-    int numCols = mat.ncol();
-    std::vector<std::vector<double>> cppMat(numRows, std::vector<double>(numCols));
-
-    for (int r = 0; r < numRows; ++r) {
-        for (int c = 0; c < numCols; ++c) {
-            cppMat[r][c] = mat(r, c);
-        }
-    }
+    std::vector<std::vector<double>> cppMat = mat_r2std(mat, true);
 
     // Call the distance function
     std::vector<std::vector<double>> distm = Dist::Dist(cppMat, method, na_rm);
 
-    // Convert std::vector<std::vector<double>> to Rcpp::NumericMatrix
-    int rows = distm.size();
-    int cols = distm[0].size();
-    Rcpp::NumericMatrix result(rows, cols);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            result(i, j) = distm[i][j];
-        }
-    }
-
-    return result;
+    // Convert std::vector<std::vector<double>> to Rcpp::NumericMatrix and return
+    return mat_std2r(distm, true);
 }
 
 // Wrapper function to compute a row-wise distance matrix for an input matrix subset
