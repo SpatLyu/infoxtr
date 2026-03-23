c5bd6c6870cff995249ae8c61f403ba63dac5627
SpatLyu <lyu.geosocial@gmail.com>
2026-03-24 00:00:01 +0800

    implement r api for entropy measures

--- R/entropy.R
+++ R/entropy.R
@@ -7,3 +7,13 @@ je = \(data, indices, base = 2, type = c("disc", "cont"), k = 3) {
     return(RcppContJE(mat, indices, k, 0, base))
   }
 }
+
+ce = \(data, target, conds, base = 2, type = c("disc", "cont"), k = 3) {
+  type = match.arg(type)
+  mat = as.matrix(data)
+  if (type == "disc") {
+    return(RcppDiscCE(mat, target, conds, base, TRUE))
+  } else {
+    return(RcppContCE(mat, target, conds, k, 0, base))
+  }
+}
