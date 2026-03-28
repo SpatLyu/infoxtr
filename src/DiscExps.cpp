441787e0e1cfb30241c1fd828de80d78f3c00c9c
SpatLyu <lyu.geosocial@gmail.com>
2026-03-29 00:00:01 +0800

    export discretize utility

---
+++ src/DiscExps.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <cmath>
+#include <string>
+#include <limits>
+#include <numeric>
+#include <algorithm>
+#include "discretize.hpp"
+#include "DataTrans.h"
+
+// Wrapper function to discretize continuous numeric vector
+// [[Rcpp::export(rng = false)]]
+Rcpp::IntegerVector RcppDisc(
+    const Rcpp::NumericVector& vec,
+    int n = 5,
+    std::string method = "natural",
+    int sample_begin = 3000,
+    int sample_prob = 0.15,
+    int seed = 123456789,
+    double threshold = 0.4,
+    int iter_step = 100,
+    const Rcpp::NumericVector& breakpoints = {},
+    bool right_closed = true
+) {
+    // Convert Rcpp::NumericVector to std::vector<double>
+    std::vector<double> vec_std = Rcpp::as<std::vector<double>>(vec);
+    std::vector<double> bp_std = Rcpp::as<std::vector<double>>(breakpoints);
+
+    // Call the discretization function
+    std::vector<size_t> discv = 
+        Disc::Disc(vec_std, method, 
+                   static_cast<size_t>(std::abs(n)), 
+                   static_cast<size_t>(std::abs(sample_begin)),
+                   sample_prob,
+                   static_cast<uint64_t>(std::abs(seed)),
+                   threshold,
+                   static_cast<size_t>(std::abs(iter_step)),
+                   bp_std, right_closed);
+
+    // Convert the result back to Rcpp::IntegerVector
+    return Rcpp::wrap(discv);
+}
