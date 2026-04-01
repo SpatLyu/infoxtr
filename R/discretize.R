d18f8491c77172ffee2803977254abeb45445b1c
SpatLyu <lyu.geosocial@gmail.com>
2026-04-02 00:00:01 +0800

    implement discretization r interface

--- R/discretize.R
+++ R/discretize.R
@@ -1,5 +1,23 @@
-
-discretize = \(x, n = 5, method = 'natural', large = 3000, prop = 0.15, 
+#' Title
+#'
+#' @param x
+#' @param n
+#' @param method
+#' @param large
+#' @param prop
+#' @param seed
+#' @param thr
+#' @param iter
+#' @param bps
+#' @param right_closed
+#'
+#' @returns A discretized integer vector.
+#' @export
+#'
+#' @examples
+#' infocaus::discretize(1:100, n = 5, method = 'natural')
+#'
+discretize = \(x, n = 5, method = 'natural', large = 3000, prop = 0.15,
                seed = 123456789, thr = 0.4, iter = 100, bps = NULL, right_closed = TRUE){
   if (any(inherits(x,'factor'),inherits(x,'character'))){
     return(as.integer(as.factor(x)))
