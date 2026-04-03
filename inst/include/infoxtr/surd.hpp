f68aa4726786466d6d40cc181e331f2a29c06b03
SpatLyu <lyu.geosocial@gmail.com>
2026-04-04 00:00:01 +0800

    draft surd utility

--- inst/include/infoxtr/surd.hpp
+++ inst/include/infoxtr/surd.hpp
@@ -8,9 +8,12 @@
 #include <limits>
 #include <thread>
 #include <cstdint>
-#include <algorithm>
 #include <numeric>
+#include <algorithm>
 #include <stdexcept>
+#include "infoxtr/combn.hpp"
+#include "infoxtr/numericutils.hpp"
+#include <RcppThread.h>
 
 namespace infoxtr 
 {
@@ -48,18 +51,53 @@ namespace surd
         size_t threads = 1,
         double base = 2.0,
         bool normalize = false)
+    {
+        if (threads == 0) threads = 1;
+        size_t hw = std::thread::hardware_concurrency();
+        if (hw > 0) threads = std::min(threads, hw);
 
-    /*****************************************************************
-     * Synergistic-Unique-Redundant Decomposition for Continuous Data
-     *****************************************************************/
-    inline SURDRes surd(
-        const ContMat& mat,
-        size_t max_order = std::numeric_limits<size_t>::max(),
-        size_t k = 3,
-        size_t alg = 0,
-        size_t threads = 1,
-        double base = 2.0,
-        bool normalize = false)
+        SURDRes result;
+
+        if (mat.size() < 2) return result;
+
+        const size_t n_vars = mat.size();
+        const size_t n_sources = mat.size() - 1;
+        max_order = std::min(max_order, n_sources);
+
+        // Construct variable combination vector
+        std::vector<size_t> ag_idx(n_sources.size());
+        std::iota(ag_idx.begin(), ag_idx.end(), 1);
+        const std::vector<std::vector<size_t>> combs =
+            infoxtr::combn::genSubsets(ag_idx, max_order);
+
+        // Compute joint entropies
+        std::vector<double> H_sources(combs.size(), 
+                                      std::numeric_limits<double>::quiet_NaN());
+        std::vector<double> H_joint(combs.size(), 
+                                    std::numeric_limits<double>::quiet_NaN());
+        double H_target = infoxtr::infotheo::je(mat, {0}, base, true);
+
+        for (size_t i = 0; i < combs.size(); ++i)
+        {   
+            std::vector<size_t> joint_idx = {0};
+            joint_idx.insert(joint_idx.end(), combs[i].begin(), combs[i].end());
+            H_sources[i] = Infoxtr::infotheo::je(mat, combs[i], base, true);
+            H_joint[i] = Infoxtr::infotheo::je(mat, joint_idx, base, true);
+        }
+            
+    }
+
+    // /*****************************************************************
+    //  * Synergistic-Unique-Redundant Decomposition for Continuous Data
+    //  *****************************************************************/
+    // inline SURDRes surd(
+    //     const ContMat& mat,
+    //     size_t max_order = std::numeric_limits<size_t>::max(),
+    //     size_t k = 3,
+    //     size_t alg = 0,
+    //     size_t threads = 1,
+    //     double base = 2.0,
+    //     bool normalize = false)
     
 } // namespace surd
 
