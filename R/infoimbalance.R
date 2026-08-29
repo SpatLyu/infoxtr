1acbfd12c2edeb01640e28947cc21622f6994188
SpatLyu <lyu.geosocial@gmail.com>
2026-08-30 00:00:01 +0800

    draft information imbalance r wrapper

--- R/infoimbalance.R
+++ R/infoimbalance.R
@@ -1,3 +1,12 @@
+info_imbalance = \(mx, my, lib = NULL, pred = NULL,
+                   k = 3, threads = 1, method = "euclidean") {
+  if (is.null(lib)) lib = seq_len(nrow(mx))
+  if (is.null(pred)) pred = lib
+  return(RcppInfoImbalance(as.matrix(mx), as.matrix(my), 
+                           abs(lib), abs(pred),
+                           abs(k), abs(threads), method))
+}
+
 #' Information Imbalance Gain
 #'
 #' @param mx Numeric matrix of hypothesised driving variable measurements.
@@ -25,6 +34,7 @@ imbalance_gain = \(mx, my, alpha = seq(0,1,0.1), lib = NULL, pred = NULL,
                    h = 1, k = 3, threads = 1, method = "euclidean") {
   if (is.null(lib)) lib = seq_len(nrow(mx))
   if (is.null(pred)) pred = lib
-  return(RcppImbalanceGain(as.matrix(mx), as.matrix(my), abs(alpha), abs(lib), abs(pred),
+  return(RcppImbalanceGain(as.matrix(mx), as.matrix(my), 
+                           abs(alpha), abs(lib), abs(pred),
                            abs(h), abs(k), abs(threads), method))
 }
